54560e605b92ec7add50676bdfd6d566a11e049d
Bohdan <yashbon@ukr.net>
2023-07-18 11:22:18 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,20 +3,24 @@ import css from './ContactList.module.css';
 const ContactList = props => {
     const { contacts, deleteContact } = props;
     return (
-        <ul className={css.contactList}>
-            {contacts.map(contact => (
-                <li className={css.contactListItem} key={contact.id}>
-                    {contact.name} {contact.number}
-                    <button
-                        className={css.deleteContact_Button}
-                        type="button"
-                        onClick={() => deleteContact(contact.id)}
-                    >
-                        delete
-                    </button>
-                </li>
-            ))}
-        </ul>
+        <>
+            {contacts.length > 0 && (
+                <ul className={css.contactList}>
+                    {contacts.map(contact => (
+                        <li className={css.contactListItem} key={contact.id}>
+                            {contact.name} {contact.number}
+                            <button
+                                className={css.deleteContact_Button}
+                                type="button"
+                                onClick={() => deleteContact(contact.id)}
+                            >
+                                delete
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </>
     );
 };
 
